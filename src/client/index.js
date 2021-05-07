d3dbe40abec2bc105fde55802bafe2f05a340e33
Samy Nassar <samy0ayman@gmail.com>
2021-05-07 18:12:31 +0200

    Initial Commit

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
+
+
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+console.log("CHANGE!!");
